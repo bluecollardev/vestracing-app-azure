809abe1dff26d5ee5515af1ef48c944e20ff4c25
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-04-21 19:53:35 -0700

    updated appcenter secret codepush

--- platforms/android/app/src/main/assets/www/admin/app/app.splitter.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    /*$('#horizontal').kendoSplitter({
-        panes: [
-			{ collapsible: true, collapsed: true, scrollable: false, resizable: true, size: '18%' },
-			{ collapsible: false, scrollable: false },
-			{ collapsible: true, collapsed: true, scrollable: false, resizable: true, size: '20%' }
-		]
-    });*/
-});
